34c66a522b5115d96732a4409c411c17441eed2f
mvieira1 <110780447+mvieira1@users.noreply.github.com>
2022-12-22 19:44:51 +0000

    Add files via upload

---
+++ README-template.md
@@ -0,0 +1,90 @@
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6).
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Built with](#built-with)
+- [What I learned](#what-i-learned)
+- [Continued development](#continued-development)
+- [Useful resources](#useful-resources)
+- [Screenshot](#screenshot)
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+  (Solution available for Mobile View (<=480px) Desktop View (>=1201px))
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Switch the product image to the next or previous on, by clicking on the buttons
+- Add items to the cart
+- View the cart and remove items from it
+
+### Screenshot
+
+![](screenshots/screenshot-desktop-view.PNG)
+![](screenshots/screenshot-desktop-view-active-states-empty-cart.PNG)
+![](screenshots/screenshot-desktop-view-active-states-filled-cart.PNG)
+![](screenshots/screenshot-desktop-view-lightbox.PNG)
+
+![](screenshots/screenshot-mobile-view.PNG)
+![](screenshots/screenshot-mobile-view-empty-cart.PNG)
+![](screenshots/screenshot-mobile-view-filled-cart.PNG)
+![](screenshots/screenshot-mobile-view-navbar.PNG)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktop-first workflow
+
+### What I learned
+
+- How to build a full-length navbar with a smooth translation;
+- How to apply filter effects to an element (ex., an svg image);
+- How to target different screen sizes, by checking the media queries, in JavaScript (MediaQueryList, matches property and 'change' event (not used, but learned));
+
+```js
+//ANTES
+const mediaQueryList = window.matchMedia('(max-width: 480px)');
+
+mediaQueryList.onchange = (eventObject) => {
+  //quando passo para uma media query diferente (quando estou a testar a responsiveness por emulação dos vários dispositivos, aumentando/diminuindo a width do ecrã), o evento 'change' é disparado*
+  if(eventObject.matches === true){
+    //code goes here
+    //console.log(eventObject);
+    //MediaQueryListEvent {isTrusted: true, media: '(max-width: 480px)', matches: true, ...}
+    //*portanto, se ultrapassar 2 media queries, vai imprimir 2 vezes o eventObject.
+  }else{
+    //code goes here
+  };
+};
+
+//DEPOIS
+const mediaQueryList = window.matchMedia('(max-width: 480px)');
+//console.log(mediaQueryList);
+//MediaQueryList {media: '(max-width: 480px)', matches: true, onchange: null}
+
+if(mediaQueryList.matches === true){
+  //code goes here - do not display lightbox
+}else{
+  //code goes here - display lightbox
+};
+
+
+//NOTA: Alterei o código para, no lugar de detetar alterações de media queries/dimensões do ecrã, detetar, simplesmente, em que range é que a dimensão atual se encontra. Esta opção faz mais sentido para mim, visto que: (1) na realidade, não posso esticar a janela de visualização do meu dispositivo. Podia simular as transições, num teste de responsiveness, usando as ferramentas de programador, mas isso não é muito relevante para o utilizador comum. (2) ao usar o evento 'change', só quando passasse por um limite de uma media query é que ia testar se a width do meu ecrã estava dentro ou não.. ou seja, se abrisse a página no Desktop, não ia ver lightbox nenhuma pq o display ia continuar set to none. Assim (conforme o código está), se abre a página do tlmv, não vê lightbox; se abre no pc, vê lightbox e pronto. Só não me posso esquecer, enquanto programadora, de quando for testar a responsividade ter de atualizar a página. Pq se abro no desktop e depois diminuir o ecrã para mobile, se clicar na imagem, a lightbox vai aparecer na mesma pq a página foi aberta em desktop (e vice-versa). O user não vai ter este ""problema"".
+```
+
+### Continued development
+- An equivalent solution for iPad, tablet & other small screens
+
+### Useful resources
+
+- [Convert Hex colors to CSS filter](https://isotropic.co/tool/hex-color-to-css-filter/) - This helped me to change the color of an svg image, whenever the user hovers over it, without needing to create a copy with a different fill
+- [MediaQueryList and 'change' event](https://www.w3schools.com/howto/howto_js_media_queries.asp) and (https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/change_event)
---
+++ script.js
@@ -0,0 +1,277 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //NAVBAR
+    const burger = document.querySelector('.burger-menu');
+    const navOptions = document.querySelector('.nav-options');
+    const closeMenu = document.querySelector('.close-menu');
+    const blackFilter = document.querySelector('.black-filter');
+
+    burger.addEventListener('click', () => {
+        blackFilter.style.display = 'block';
+        navOptions.style.left = '0'; //sliding navbar
+    });
+
+    closeMenu.addEventListener('click', () => {
+        blackFilter.style.display = 'none';
+        navOptions.style.left = '-50%'; //sliding it back :')
+    });
+
+
+    //IMAGES GALERY
+    const galeryImages = document.querySelectorAll('.galery img');
+    const biggerImage = document.querySelector('.bigger-image');
+
+    galeryImages.forEach(image => {
+        image.addEventListener('click', () => {
+            galeryImages.forEach(image => {
+                image.classList.remove('selected');
+            });
+            image.classList.add('selected');
+            let number = image.getAttribute('src').charAt(21);
+            biggerImage.setAttribute('src', `images/image-product-${number}.jpg`);
+        });
+    });
+
+    /* A src de cada uma das imagens da galeria é "images/image-product-[1|2|3|4]-thumbnail.jpg", enquanto que a imagem maior correspondente está na path "images/image-product-[1|2|3|4].jpg". As duas versões da mesma imagem têm o mesmo número */
+
+
+    //HOW MANY ITEMS DO WE WANT?
+    const minus = document.querySelector('.minus');
+    const plus = document.querySelector('.plus');
+
+    const span = document.querySelector('.count span');
+
+    let count = Number(span.innerText); //vai buscar o 0 que já lá está <=> let count = 0
+    minus.addEventListener('click', () => {
+        switch(count === 0){
+            case true:
+                break; //se a count for igual a zero, não podemos descer mais e comprar pares negativos de sapatilhas...
+            case false:
+                count--;
+                break;
+        };
+        span.innerText = count;
+    });
+
+    plus.addEventListener('click', () => {
+        count++;
+        span.innerText = count;
+    });
+
+
+    //ADD TO CART
+    const addToCart = document.querySelector('.add-to-cart');
+    const cartFilled = document.querySelector('.cart-filled');
+    const cartItems = document.querySelector('.cart-items');
+    const emptyCart = document.querySelector('.empty-cart');
+
+    //NOTA:
+    //Se o utilizador adicionar 5 artigos depois de adicionar 2, por ex., a quantidade de artigos no carrinho é acumulada (5+2 = 10). Não sei se era isso que pretendiam com o desafio, mas faz mais sentido para mim do que substituir o número de artigos anterior.
+    //Em E-commerce sites, como Amazon, por ex., é assim que funciona também.
+
+    let acc = 0;
+    addToCart.addEventListener('click', () => {
+        //se o user tiver selecionado, pelo menos, 1 par de sapatilhas
+        if(span.innerText !== '0'){
+            acc = acc + +span.innerText;
+            //aparece uma bolinha em cima do carrinho com o número de items selecionados
+            cartFilled.innerText = acc;
+            cartFilled.style.display = 'block';
+
+            //console.log(cartItems.children);
+            //se está lá o div.emptyCart, remove-se
+            if(Array.from(cartItems.children).includes(emptyCart)){
+                cartItems.removeChild(emptyCart);
+            };
+
+            //e o artigo e respetiva quantidade são adicionados ao carrinho
+            cartItems.innerHTML = `
+            
+                <div class="header">
+                    Cart
+                </div>
+
+                <div class="item">
+                    <div>
+                        <img src="images/image-product-1-thumbnail.jpg" alt="product" class="product">
+                    </div>
+
+                    <div class="text">
+                        <p>Fall Limited Edition Sneakers</p>
+                        <p>$125.00 x <span class="items-number">${acc}</span> <span class="price">$${+acc * 125}.00</span></p>
+                    </div>
+
+                    <div>
+                        <img src="images/icon-delete.svg" alt="trash-icon" class="delete">
+                    </div>
+                    
+                </div>
+
+                <div class="checkout">
+                    Checkout
+                </div>
+            `
+
+            const item = document.querySelector('.item');
+            const checkout = document.querySelector('.checkout');
+
+            //DELETE ITEMS
+            const trashIcon = document.querySelector('.delete');
+            trashIcon.addEventListener('click', () => {
+                //o carrinho fica vazio
+                cartItems.removeChild(item);
+                cartItems.removeChild(checkout);
+                cartItems.appendChild(emptyCart);
+                //e a bolinha com o número de artigos desaparece
+                cartFilled.style.display = 'none';
+                //o acc retorna a 0 (necessário, pq, caso contrário, ia registar a qtd de artigos adicionada antes de terem sido removidos e contar a partir daí)
+                acc = 0;
+            });
+        };
+    });
+
+
+    //SHOW CART ITEMS
+    const shoppingCart = document.querySelector('.shopping-cart');
+
+    shoppingCart.addEventListener('click', () => {
+        cartItems.classList.toggle('display-none');
+    });
+
+
+    //LIGHTBOX
+    const lightbox = document.querySelector('.lightbox');
+
+    const mediaQueryList = window.matchMedia('(max-width: 480px)');
+    //console.log(mediaQueryList);
+    //MediaQueryList {media: '(max-width: 480px)', matches: true, onchange: null}
+   
+    if(mediaQueryList.matches === true){ //se estou em Mobile (até 480px, inclusive)
+        lightbox.style.display = 'none'; //não há lightbox
+
+    }else{
+        //se não, estamos noutro screen size (neste caso, a solução foi desenvolvida apenas para Mobile e  Desktop, mas as instruções deste block aplicam-se a todas as screen widths a cima de 480px)
+
+        const lightboxImage = document.querySelector('.lightbox-image');
+        const thumbnailImages = document.querySelectorAll('.thumbnail img');
+
+        biggerImage.addEventListener('click', (eventObject) => {
+            //console.log(eventObject);
+            //a imagem que aparece na lightbox é a mesma em que o user clicou no main
+            lightboxImage.setAttribute('src', eventObject.target.getAttribute('src'));
+            //e a imagem da thumbnail equivalente aparece selecionada
+            let imgNb = eventObject.target.getAttribute('src').charAt(21);
+            thumbnailImages.forEach(image => {
+                image.classList.remove('selected');
+                if(image.getAttribute('src').charAt(21) === imgNb){
+                    image.classList.add('selected');
+                };
+            });
+            lightbox.style.display = 'flex';
+        });
+
+        const closeLightbox = document.querySelector('.close-lightbox');
+        closeLightbox.addEventListener('click', () => {
+            lightbox.style.display = 'none';
+        });
+
+
+        //--NEXT & PREVIOUS IMAGE
+        const next = document.querySelector('.next');
+        const previous = document.querySelector('.previous');
+
+        next.addEventListener('click', (eventObject) => {
+
+            //console.log(eventObject.target.previousSibling.previousSibling) podia ter ido por aqui também
+            let currentImg = eventObject.target.parentNode.children[0]; //imagem selecionada
+            //vai buscar o número da imagem; podia ter utilizado charAt também, mas vou usar o método slice para relembrar
+            let currentNb = currentImg.getAttribute('src').slice(-5,-4);
+
+            if(currentNb !== '4'){
+                let nextNb = +currentNb + 1;
+                currentImg.setAttribute('src', `images/image-product-${nextNb}.jpg`);
+
+                //deselicona a imagem da thumbnail anterior e seleciona a equivalente à imagem atual
+                thumbnailImages.forEach(image => {
+                    image.classList.remove('selected');
+                    if(image.getAttribute('src').charAt(21) == nextNb){ // == em vez de === para NÃO comparar o datatype; neste caso, o charAt(21) é uma string e o nextNb é um integer. Podia também ter feito a conversão
+                        image.classList.add('selected');
+                    };
+                });
+            };
+        });
+
+        previous.addEventListener('click', (eventObject) => {
+            let currentImg = eventObject.target.parentNode.children[0];
+            //console.log(currentImg);
+            let currentNb = currentImg.getAttribute('src').slice(-5,-4);
+            if(currentNb !== '1'){ 
+                let previousNb = +currentNb - 1;
+                currentImg.setAttribute('src', `images/image-product-${previousNb}.jpg`);
+
+                thumbnailImages.forEach(image => {
+                    image.classList.remove('selected');
+                    if(image.getAttribute('src').charAt(21) == previousNb){
+                        image.classList.add('selected');
+                    };
+                });
+            };
+        });
+
+
+        //--THUMBNAIL IMAGES - alterar a lightbox image quando clico nas imagens da thumbnail (mesmo algoritmo da secção IMAGES GALERY)
+        thumbnailImages.forEach(image => {
+            image.addEventListener('click', () => {
+                thumbnailImages.forEach(image => {
+                    image.classList.remove('selected');
+                });
+                image.classList.add('selected');
+                let number = image.getAttribute('src').charAt(21);
+                lightboxImage.setAttribute('src', `images/image-product-${number}.jpg`);
+            });
+        });
+    };
+});
+
+/*Em alternativa ao código que começa das linhas 79 à 104, poderia ter feito
+
+    const item = document.createElement('div');
+    item.classList.add('item');
+
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    img.setAttribute('src', 'images/image-product-1-thumbnail.jpg');
+    img.setAttribute('alt', 'product');
+    img.classList.add('product');
+    div.appendChild(img);
+
+    item.appendChild(div);
+
+    const text = document.createElement('div');
+    text.classList.add('text');
+    const p = document.createElement('p');
+    p.innerText = 'Fall Limited Edition Sneakers';
+    const p2 = document.createElement('p');
+    p2.innerHTML = `
+        $125.00 x <span class="items-number">${span.innerText}</span> <span class="price">$${+span.innerText * 125}.00</span>
+    `; //aqui, não estaria a acumular o número de artigos, mas sim a substituir. Não sei o que pretendiam. Para mim, faz mais sentido a primeira, mas deixo as duas opções.
+    text.appendChild(p);
+    text.appendChild(p2);
+    item.appendChild(text);
+
+    const div2 = document.createElement('div');
+    const img2 = document.createElement('img');
+    img2.setAttribute('src', 'images/icon-delete.svg');
+    img2.setAttribute('alt', 'trash-icon');
+    img2.classList.add('delete');
+    div2.appendChild(img2);
+    item.appendChild(div2);
+
+    const checkout = document.createElement('div');
+    checkout.classList.add('checkout');
+    checkout.innerText = 'Checkout';
+
+    cartItems.appendChild(item);
+    cartItems.appendChild(checkout);
+*/
